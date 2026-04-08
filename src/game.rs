82bd186c2f547ffe378309529218392150d375ae
wren <kinnickb@ymail.com>
2026-04-07 23:51:01 -0400

    Board initialization

--- src/game.rs
+++ src/game.rs
@@ -1,3 +1,5 @@
+use rand::prelude::*;
+
 pub struct GameState {
     pub board: [ [u32; 4]; 4],
     score: u8
@@ -5,6 +7,25 @@ pub struct GameState {
 
 impl GameState {
     pub fn new() -> GameState {
-        GameState { board: [[0,0,0,2], [2,0,0,2], [0,4,8,2], [16,4,0,0]], score: 0}
+        GameState { board: init_board(), score: 0}
     }
 }
+
+fn init_board() -> [[u32; 4]; 4] {
+    let mut board: [[u32; 4]; 4] = [[0; 4]; 4];
+    let (row, col) = get_random_tile(&board);
+    board[row][col] = if rand::random_range(0..100) < 90 { 2 } else { 4 };
+    let (row, col) = get_random_tile(&board);
+    board[row][col] = if rand::random_range(0..100) < 90 { 2 } else { 4 };
+    board
+}
+
+fn get_random_tile(board: &[[u32; 4]; 4]) -> (usize, usize) {
+    let row = rand::random_range(0..4);
+    let col = rand::random_range(0..4);
+    if board[row][col] != 0 {
+        get_random_tile(board)
+    } else {
+        (row, col)
+    }
+} 
